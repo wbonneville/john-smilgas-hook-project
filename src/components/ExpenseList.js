0fb591e025196da61d9397c1ef4abf77e37dd5d1
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-20 15:30:42 -0500

    Add id, charge, amt and icons

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Item from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
 
 export const ExpenseList = ({ expenses }) => {
   return (
@@ -9,7 +10,12 @@ export const ExpenseList = ({ expenses }) => {
           return <Item key={expense.id} expense={expense}></Item>;
         })}
       </ul>
-      {expenses.length > 0 && <button className="btn">clear expenses</button>}
+      {expenses.length > 0 && (
+        <button className="btn">
+          clear expenses
+          <MdDelete className="btn-icon"></MdDelete>
+        </button>
+      )}
     </>
   );
 };
